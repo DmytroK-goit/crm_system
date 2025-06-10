17d6dc09741fac7c40af6d6ae6e430cbaa9f5a6b
Dmytro Kovbasiuk <159040488+DmytroK-goit@users.noreply.github.com>
2025-06-10 22:14:42 +0300

    fixed bugs

--- src/app/components/input-field.tsx
+++ src/app/components/input-field.tsx
@@ -3,24 +3,45 @@
 import React from 'react';
 import { Field } from 'formik';
 
-export interface InputFieldProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {
+export interface InputFieldProps {
   label?: string;
+  name: string;
+  id?: string;
+  placeholder?: string;
+  required?: boolean;
+  type?: string;
+  as?: 'input' | 'select' | 'textarea';
+  children?: React.ReactNode;
 }
 
-export default function InputField({ label, id, ...rest }: InputFieldProps) {
+export default function InputField({
+  label,
+  id,
+  name,
+  placeholder,
+  required = false,
+  type = 'text',
+  as = 'input',
+  children,
+}: InputFieldProps) {
   return (
     <div className="flex flex-col">
       {label && (
-        <label htmlFor={id} className="mb-2 text-base color-gray-900">
+        <label htmlFor={id ?? name} className="mb-2 text-base text-gray-900">
           {label}
+          {required && <span className="text-red-500"> *</span>}
         </label>
       )}
       <Field
-        {...rest}
-        id={id}
+        as={as}
+        id={id ?? name}
+        name={name}
+        placeholder={placeholder}
+        type={as === 'input' ? type : undefined}
         className="p-3 h-11 text-sm rounded border border-gray-300 shadow"
-      />
+      >
+        {children}
+      </Field>
     </div>
   );
 }
